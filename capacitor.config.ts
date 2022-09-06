d152d9771d9ff3cf88fddeb5280a0d92c0204565
Dan Jarvis <danwakeem@gmail.com>
2022-09-06 08:50:12 -0500

    Updated app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'baby-kick-counter',
+  appName: 'kick-counter',
   webDir: 'build',
   bundledWebRuntime: false
 };
