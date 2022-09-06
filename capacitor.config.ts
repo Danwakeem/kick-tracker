0aa27321a112f0e6f4d3af9b42c379a3bd0fab47
Dan Jarvis <danwakeem@gmail.com>
2022-09-06 08:56:00 -0500

    Updated favicon and web icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.danwakeem.kick-counter',
   appName: 'kick-counter',
   webDir: 'build',
   bundledWebRuntime: false
